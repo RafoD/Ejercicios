2251f703ed42a8babfe27471fe62a530c8747187
Rafael Alonso David Peñalva <37584758+RafoD@users.noreply.github.com>
2018-04-18 10:02:58 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,168 +1,88 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    //Ejercicio 1
-    int edad;
-    cout<<"Ejercicio 1"<<endl;
-    cout<<"Ingrese su edad: ";
-    cin>>edad;
-    if(edad>=18)
-        cout<<"Usted es mayor de edad"<<endl;
-    else{
-        cout<<"Usted es menor de edad"<<endl;
+void imprimir(int a[],int tamanio){
+    for(int i = 0; i<tamanio; i++){
+        cout<<a[i]<<" ";
     }
     cout<<endl;
-    //Ejercicio 2
-    int n;
-    cout<<"Ejercicio 2"<<endl;
-    cout<<"Ingrese un n: ";
-    cin>>n;
-    for(int i=1; i<n; i++){
-        cout<<i<<", ";
+}
+//suma
+int suma(int arr[], int tamanio){
+    int sum = 0;
+    for(int i=0; i<tamanio;i++){
+        sum+=arr[i];
     }
-    cout<<n<<endl;
-    cout<<endl;
+    return sum;
+}
 
-    //Ejercicio 3
-    int n_1;
-    int mayor, menor;
-    float suma;
-    cout<<"Ejercicio 3"<<endl;
-    cout<<"Ingrese 3 numeros: "<<endl;
-    cout<<"Numero (1): "; cin>>n_1;
-    mayor = n_1;
-    menor = n_1;
-    suma = n_1;
-    for(int i=1; i<3; i++){
-        cout<<"Numero ("<<i+1<<"): ";
-        cin>>n_1;
-        suma += n_1;
-        mayor = mayor<n_1? n_1 : mayor;
-        menor = menor>n_1? n_1 : menor;
+int suma_1(int arr[], int tamanio){
+    if(tamanio==1)
+        return arr[0];
+    tamanio--;
+    return arr[tamanio] + suma_1(arr,tamanio);
+}
+//invertir
+void invertir(int arr[], int tamanio){
+    int izquierda = 0;
+    int derecha = tamanio-1;
+    while(izquierda<=derecha){
+        int aux = arr[izquierda];
+        arr[izquierda] = arr[derecha];
+        arr[derecha] = aux;
+        izquierda++;
+        derecha--;
     }
-    cout<<"El mayor es :"<<mayor<<endl;
-    cout<<"El menor es :"<<menor<<endl;
-    cout<<"El promedio es: "<<suma/3<<endl;
-    cout<<endl;
+}
 
-    //Ejercicio 4
-    int a,b;
-    cout<<"Ejercicio 4"<<endl;
-    cout<<"Ingrese dos numeros: "<<endl;
-    cout<<"Numero (1): "; cin>>a;
-    cout<<"Numero (2): "; cin>>b;
-    if(!(a%2) && !(b%2))
-        cout<<"Ambos numeros son multiplos de dos"<<endl;
-    if(!(a%b))
-        cout<<"El primer numero es multiplo del segundo"<<endl;
-    if(a*a == b){
-        cout<<"El primer numero al cuadrado tiene como resultado el segundo"<<endl;
-    }
-    cout<<endl;
-
-    //Ejercicio 5
-    cout<<"Ejercicio 5"<<endl;
-    cout<<"Ingrese un numero: ";
-    int numero;
-    cin>>numero;
-    int contador = 0;
-    int tmp = 2;
-    cout<<endl;
-    while((contador == 0)&&(tmp<numero)){
-        if(!(numero%tmp)){
-            contador++;
-        }
-        tmp++;
-    }
-    if(contador)
-        cout<<"El numero no es primo"<<endl;
-    else{
-        cout<<"El numero es primo"<<endl;
-    }
-    cout<<endl;
-
-    //Ejercicio 6
-    cout<<"Ejercicio 6"<<endl;
-    int n;
-    cout<<"Ingrese un numero: "; cin>>n;
+//tamaño
+int tamanioDe(char cadena[]){
     int c=0;
-    numero = 1;
-    while(c<n){
-        contador = 0;
-        ++numero;
-        for(int i=2;i<numero;i++){
-            contador = !(numero%i)? contador++ : 0;
-        }
-        if(!contador){
-            cout<<numero<<" ";
-            c++;
-        }
+    while(cadena[c]!='\0'){
+        c++;
     }
-    cout<<endl;
-    cout<<endl;
-
-    //Ejercicio 7
-    cout<<"Ejercicio 7"<<endl;
-    cout<<"Ingrese un numero: "; cin>>numero;
-    for(int i=10000; i>=1; i/=10){
-        tmp = numero;
-        cout<<tmp/i<<"\t";
-        numero %= i;
+    return c;
+}
+//copiar
+void copiar(char a[], char b[]){
+    for(int i=0; i<tamanioDe(b);i++){
+        a[i] = b[i];}
+}
+void copiar_2(char a[], char b[],int aux=0){
+    if(aux > tamanioDe(b))
+        return;
+    a[aux] = b[aux];
+    aux++;
+    return copiar_2(a,b,aux);
+}
+//concatenar
+void concatenar(char a[], char b[]){
+    int t = tamanioDe(a);
+    int t_2 = tamanioDe(b);
+    for(int i = 0; i<t_2; i++){
+        a[t] = b[i];
+        t++;
     }
-    cout<<endl;
-    cout<<endl;
+}
 
-    //Ejercicio 8
-    cout<<"Ejercicio 8"<<endl;
-    cout<<"Ingrese un numero: "; cin>>numero;
-    int tmp2;
-    tmp = numero/1000;
-    tmp2 = numero % 100;
-    tmp2 = (tmp2 % 10)*10 + (tmp2/10);
 
-    if(tmp2 == tmp)
-        cout<<"El numero es palindrome"<<endl;
-    else{
-    cout<<"El numero no es palindrome"<<endl;}
-    cout<<endl;
 
-    //Ejercicio 9
-    int anio;
-    cout<<"Ejercicio 9"<<endl;
-    cout<<"Ingrese un anio : ";cin>>anio;
-    if(!(anio%4) && (anio%100) || !(anio%400)){
-         cout<<"El anio es bisiesto ";
-    }else{
-         cout<<"El anio no es bisiesto ";
-    }
-
-    cout<<endl;
-    cout<<endl;
-
-    //Ejercicio 10
-    cout<<"Ejercicio 10"<<endl;
-    cout<<"Ingrese un numero: ";cin>>numero;
-    tmp = 1;
-    tmp2 = 0;
-    suma = 1;
-    for(int i = 0;i<numero;i++){
-        suma = tmp + tmp2;
-        tmp = tmp2;
-        tmp2 = suma;
-        cout<<tmp2<<" ";
-    }
-    cout<<endl;
-    cout<<endl;
-
-    //Ejercicio 11
-    cout<<"Ejercicio 11"<<endl;
-    cout<<"Ingrese un numero: ";cin>>numero;
-    int factorial = 1;
-    for(int i=2; i<=numero; i++){
-        factorial *=i;
-    }
-    cout<<factorial<<endl;
-
+int main(){
+    int arr[] = {1,2,3,4,5,6};
+    //suma
+    cout<<suma(arr,6)<<endl;
+    //invertir
+    invertir(arr,6);    imprimir(arr,6);
+    char cad[] = "jajajaja";
+    char cad_1[] = "jejejeje";
+    char cad_2[13] = "";
+    //tamaño
+    cout<<tamanioDe(cad)<<endl;
+    //copiar
+    copiar(cad_2,cad);
+    cout<<cad_2<<endl;
+    //concatenar
+    concatenar(cad,cad_1);
+    cout<<cad;
     return 0;
 }
